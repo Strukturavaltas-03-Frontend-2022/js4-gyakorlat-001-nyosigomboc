e9c864901f9d977b31ff8950c113abd7ec9dcc93
Gyozo Miklos <nyosdroid@gmail.com>
2022-09-09 03:39:18 +0200

    done

--- 13-promise/exercises/02/solution/js/sortStrings.js
+++ 13-promise/exercises/02/solution/js/sortStrings.js
@@ -1,1 +1,15 @@
+const allString = (arr = ['']) => !Array.isArray(arr) || arr.every((item) => typeof item === 'string');
+
+const sortStrings = (arr = ['']) => new Promise((resolve, reject) => {
+  if (!allString(arr)) {
+    reject(new Error('Error: Not all items in the array are strings!'));
+    return;
+  }
+  try {
+    resolve(arr.sort());
+  } catch (err) {
+    reject(new Error('Error: Something went wrong with sorting words!'));
+  }
+});
+
 export default sortStrings;
